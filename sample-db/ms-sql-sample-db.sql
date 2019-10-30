e79166d8069bcd67f4fbed5290767411498c4a13
limelime <limelime@users.noreply.github.com>
2019-10-30 10:32:32 -0400

    Add sample db.

---
+++ sample-db/ms-sql-sample-db.sql
@@ -0,0 +1,32 @@
+--DROP DATABASE Test;
+CREATE DATABASE Test;
+GO
+
+-- Create tables.
+USE Test;
+CREATE TABLE Emails(
+        Contact_id INT NOT NULL,
+        Email NCHAR(50) NOT NULL,
+        Label NCHAR(50) NOT NULL,
+        CONSTRAINT PK_Emails PRIMARY KEY (Contact_id, Email)
+    );
+
+CREATE TABLE Contacts(
+        id INT IDENTITY(2,1) PRIMARY KEY, -- Auto increment by 1 starting from 2.
+        Firstname NCHAR(50) NOT NULL, 
+        Lastname NCHAR(50) NOT NULL
+    );
+
+-- Insert data.
+USE Test;
+INSERT INTO Contacts(Firstname, Lastname) VALUES('Joe', 'Smith');
+    INSERT INTO Emails(Contact_id, Email, Label) VALUES(SCOPE_IDENTITY(), 'joe.smith@OpenWritings.net', 'personal'); 
+INSERT INTO Contacts(Firstname, Lastname) VALUES('Anne', 'Bond');
+    INSERT INTO Emails(Contact_id, Email, Label) VALUES(SCOPE_IDENTITY(), 'anne.bond@OpenWritings.net', 'work'); 
+INSERT INTO Contacts(Firstname, Lastname) VALUES('James', 'Bond');
+    INSERT INTO Emails(Contact_id, Email, Label) VALUES(SCOPE_IDENTITY(), 'james.bond@OpenWritings.net', 'work');    
+
+-- Display tables content.
+USE Test;
+SELECT * FROM Contacts;
+SELECT * FROM Emails;
